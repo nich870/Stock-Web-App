8aa58bec21f133aa120062754470e67f6bfb29fc
nich870 <gebauernicholas@gmail.com>
2026-07-15 14:30:32 -0500

    Trying new windows strategy (30).

--- app.py
+++ app.py
@@ -79,8 +79,8 @@ if app_mode == "Market Scanner":
                     if data.empty: continue
                     
                     # Math Indicators block (Support, Resistance, RSI, ATR)
-                    data["Support"] = data["Close"].rolling(window=20).min()
-                    data["Resistance"] = data["Close"].rolling(window=20).max()
+                    data["Support"] = data["Close"].rolling(window=30).min()
+                    data["Resistance"] = data["Close"].rolling(window=30).max()
                     
                     delta = data["Close"].diff()
                     gain = delta.where(delta > 0, 0.0)
